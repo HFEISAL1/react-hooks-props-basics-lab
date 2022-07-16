875f13a7e4268d3403e4503f0ce0779d5ccb38d2
hfeisal1 <halima.feisal@student.moringaschool.com>
2022-07-16 02:59:12 -0700

    propsdynamics

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,19 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
+
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
+const {name, color, city, bio, links} = user
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={name} color={color} city={city} />
+      <About bio={bio} links= {links} />
+      {/* <Links linkedin={links.linkedin} github={links.github} /> */}
     </div>
   );
 }
